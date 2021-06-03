11ef76f0d10db5874dd0eb5c731042f0b4bbae3e
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-02 18:30:24 -0600

    cambio de personal a persona en algunas funciones

--- server/src/controllers/personalController.js
+++ server/src/controllers/personalController.js
@@ -14,10 +14,78 @@ const listarPersonal = (req, res) => {
 	}
 };
 
-const personalPorId = (req, res) => {
+const personaPorId = (req, res) => {
 	const {id} = req.params;
 	if (connection) {
 		let sql = "SELECT * FROM personal WHERE id = ?";
+		connection.query(sql, [id], (err, persona) => {
+			if (err) {
+				res.status(400).json(err);
+			} else {
+				console.log(persona);
+				res.json(persona[0]);
+			}
+		});
+	}
+};
+
+const agregarPersona = (req, res) => {
+	if (connection) {
+		console.log(req.body);
+		const personal = req.body;
+
+		if (!personal.nombre) {
+			return res
+				.status(400)
+				.send({ok: false, mensaje: "Campo 'nombre' no puede estar vacío."});
+		}
+		if (!personal.apellidos) {
+			return res
+				.status(400)
+				.send({ok: false, mensaje: "Campo 'apellidos' no puede estar vacío."});
+		}
+
+		const sql = "INSERT INTO personal set ?";
+
+		connection.query(sql, [personal], (err, data) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.json({ok: true, data, mensaje: "Persona creada con éxito."});
+			}
+		});
+	}
+};
+
+const editarPersona = (req, res) => {
+	if (connection) {
+		const {id} = req.params;
+		const persona = req.body;
+
+		let sql = "UPDATE personal set ? WHERE id = ?";
+
+		connection.query(sql, [persona, id], (err, data) => {
+			if (err) {
+				res.json(err);
+			} else {
+				let mensaje = "";
+				if (data.changedRows === 0) {
+					mensaje = "La información es la misma";
+				} else {
+					mensaje = "Persona actualizada con éxito.";
+				}
+
+				res.json({ok: true, data, mensaje});
+			}
+		});
+	}
+};
+
+const eliminarPersona = (req, res) => {
+	const {id} = req.params;
+	if (existePersona(id)) return res.json({mensaje: "No existe la persona."});
+	if (connection) {
+		let sql = "DELETE FROM personal WHERE id = ?";
 		connection.query(sql, [id], (err, personal) => {
 			if (err) {
 				res.status(400).json(err);
@@ -29,72 +97,20 @@ const personalPorId = (req, res) => {
 	}
 };
 
-const agregarPersonal = (req, res) => {
-	if (connection) {
-		console.log(req.body);
-		const personal = req.body;
-
-		if (!personal.nombre) {
-			return res
-				.status(400)
-				.send({error: true, mensaje: "Campo 'nombre' no puede estar vacío."});
-		}
-		if (!personal.apellidos) {
-			return res
-				.status(400)
-				.send({error: true, mensaje: "Campo 'apellidos' no puede estar vacío."});
-		}
-
-		const sql = "INSERT INTO personal set ?";
-
-		connection.query(sql, [personal], (err, data) => {
-			if (err) {
-				console.log(err);
-			} else {
-				res.json({error: false, data, mensaje: "Personal creado con éxito."});
-			}
-		});
-	}
-};
-
-const editarPersonal = (req, res) => {
-	if (connection) {
-		const {id} = req.params;
-		const personal = req.body;
-
-		let sql = "UPDATE personal set ? WHERE id = ?";
-
-		connection.query(sql, [personal, id], (err, data) => {
-			if (err) {
-				res.json(err);
-			} else {
-				let mensaje = "";
-				if (data.changedRows === 0) {
-					mensaje = "La información es la misma";
-				} else {
-					mensaje = "Personal actualizado con exito.";
-				}
-
-				res.json({error: false, data, mensaje});
-			}
-		});
-	}
-};
-
-const existePersonal = (id) => {
+const existePersona = (id) => {
 	const sql = "SELECT id FROM personal WHERE id = ?";
 	let existe = false;
 	connection.query(sql, [id], (err, data) => {
 		existe = !err;
 		console.log(data);
 	});
-	return error;
+	return existe;
 };
 
 module.exports = {
 	listarPersonal,
-	agregarPersonal,
-	editarPersonal,
-	existePersonal,
-	personalPorId,
+	agregarPersona,
+	editarPersona,
+	personaPorId,
+	eliminarPersona,
 };
