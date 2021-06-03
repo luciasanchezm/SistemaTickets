efdcd7de3325813f9d89867658d752e09d736b01
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-02 18:29:29 -0600

    eliminar persona route

--- server/src/routes/personalRoutes.js
+++ server/src/routes/personalRoutes.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const {
-	editarPersonal,
-	agregarPersonal,
+	editarPersona,
+	agregarPersona,
 	listarPersonal,
-	personalPorId,
+	personaPorId,
+	eliminarPersona,
 } = require("../controllers/personalController");
 
 const personalRoutes = express.Router();
 
 personalRoutes.get("/personal", listarPersonal);
 
-personalRoutes.get("/personal/:id", personalPorId);
+personalRoutes.get("/personal/:id", personaPorId);
 
-personalRoutes.post("/personal", agregarPersonal);
+personalRoutes.post("/personal", agregarPersona);
 
-personalRoutes.put("/personal/:id", editarPersonal);
+personalRoutes.put("/personal/:id", editarPersona);
+
+personalRoutes.delete("/personal/:id", eliminarPersona);
 
 module.exports = personalRoutes;
