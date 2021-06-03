01d1e1de1a1a3f8ab56bb9301c247def539d8efe
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-02 18:28:42 -0600

    custom table component

---
+++ client/src/components/Table.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<b-table
+			show-empty
+			:empty-text="emptyText"
+			hover
+			:busy="busy"
+			:items="items"
+			:fields="fields"
+		>
+			<template #table-busy>
+				<div class="text-center text-danger my-2">
+					<b-spinner class="align-middle"></b-spinner>
+					<strong>Loading...</strong>
+				</div>
+			</template>
+
+			// Bootstrap
+			<template #cell(actions)="data">
+				<slot name="actions" :item="data"></slot>
+			</template>
+		</b-table>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Table",
+	props: {
+		items: Array,
+		fields: Array,
+		busy: Boolean,
+		emptyText: String,
+	},
+};
+</script>
+
+<style>
+table#table-transition-example .flip-list-move {
+	transition: transform 1s;
+}
+</style>
