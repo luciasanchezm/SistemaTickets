d9bea3e7a9112f512c223cc8be388d5a7377a5c4
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-04 00:41:35 -0600

    controlador de personal

---
+++ server/src/controllers/personalController.js
@@ -0,0 +1,114 @@
+const connection = require("../config/connection");
+
+const listarPersonal = (req, res) => {
+	if (connection) {
+		let sql = "SELECT * FROM personal";
+		connection.query(sql, (err, categorias) => {
+			if (err) {
+				res.status(400).json(err);
+			} else {
+				res.json(categorias);
+			}
+		});
+	}
+};
+
+const personaPorId = (req, res) => {
+	const {id} = req.params;
+	if (connection) {
+		let sql = "SELECT * FROM personal WHERE id = ?";
+		connection.query(sql, [id], (err, persona) => {
+			if (err) {
+				res.status(400).json(err);
+			} else {
+				res.json(persona[0]);
+			}
+		});
+	}
+};
+
+const agregarPersona = (req, res) => {
+	if (connection) {
+		const personal = req.body;
+
+		if (!personal.nombre) {
+			return res
+				.status(400)
+				.send({ok: false, mensaje: "Campo 'nombre' no puede estar vacío."});
+		}
+		if (!personal.apellidos) {
+			return res
+				.status(400)
+				.send({ok: false, mensaje: "Campo 'apellidos' no puede estar vacío."});
+		}
+
+		const sql = "INSERT INTO personal set ?";
+
+		connection.query(sql, [personal], (err, data) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.json({ok: true, data, mensaje: "Persona creada con éxito."});
+			}
+		});
+	}
+};
+
+const editarPersona = (req, res) => {
+	if (connection) {
+		const {id} = req.params;
+		const persona = req.body;
+
+		let sql = "UPDATE personal set ? WHERE id = ?";
+
+		connection.query(sql, [persona, id], (err, data) => {
+			if (err) {
+				res.json(err);
+			} else {
+				let mensaje = "";
+				if (data.changedRows === 0) {
+					mensaje = "La información es la misma";
+				} else {
+					mensaje = "Persona actualizada con éxito.";
+				}
+
+				res.json({ok: true, data, mensaje});
+			}
+		});
+	}
+};
+
+const eliminarPersona = (req, res) => {
+	const {id} = req.params;
+	if (existePersona(id)) return res.json({mensaje: "No existe la persona."});
+	if (connection) {
+		let sql = "DELETE FROM personal WHERE id = ?";
+		connection.query(sql, [id], (err, personal) => {
+			if (err) {
+				res.status(400).json(err);
+			} else {
+				res.json(personal);
+			}
+		});
+	}
+};
+
+const existePersona = async (id) => {
+	const sql = "SELECT id FROM personal WHERE id = ?";
+	let existe = false;
+	await connection.query(sql, [id], (err, data) => {
+		if (!err) {
+			existe = !!data;
+		} else existe = false;
+	});
+	return existe;
+};
+
+module.exports = {
+	listarPersonal,
+	agregarPersona,
+	editarPersona,
+	personaPorId,
+	eliminarPersona,
+	existePersona,
+};
