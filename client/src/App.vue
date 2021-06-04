fdbf2cf5be0244cb97d8199191c5aa3b5aa48100
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-04 00:45:01 -0600

    initial workflow setup

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/categorias">Categorías</router-link> |
+			<router-link to="/personal">Personal</router-link> |
+			<router-link to="/tickets">Tickets</router-link>
+		</div>
+		<router-view class="container" />
+	</div>
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+#nav {
+	padding: 30px;
+}
+
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+	color: #42b983;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="home">
+		<img
+			alt="Vue logo"
+			src="https://static.vecteezy.com/system/resources/previews/001/189/271/non_2x/tickets-png.png"
+		/>
+		<h1>Bienvenido a <b>TicketWeb</b></h1>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: "Home",
+	components: {},
+};
+</script>
