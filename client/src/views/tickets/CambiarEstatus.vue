fe38a79c4d85d33bfa8da0855ab589ded18461ac
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-04 00:44:09 -0600

    seccion de tickets

---
+++ client/src/views/tickets/CambiarEstatus.vue
@@ -0,0 +1,84 @@
+<template>
+	<div>
+		<h1>Cambiar Estatus</h1>
+		<b-form @submit.prevent="guardarTicket()">
+			<Input id="id" v-model="$route.params.id" titulo="ID" class="row" disabled />
+
+			<div class="mb-3 row">
+				<label class="col-sm-2 col-form-label text-start" for="estatus">Estatus</label>
+				<div class="col-sm-10">
+					<b-form-select
+						class="form-control"
+						id="estatus"
+						v-model="ticket.estatus"
+						:options="options"
+					></b-form-select>
+				</div>
+			</div>
+
+			<b-button type="submit" class="mt-2" variant="primary">Guardar</b-button>
+		</b-form>
+	</div>
+</template>
+
+<script>
+import Vue from "vue";
+import {mapActions} from "vuex";
+import Input from "../../components/Input";
+
+export default {
+	name: "EditarTicket",
+	components: {Input},
+	data() {
+		return {
+			options: [
+				{value: "", text: "Seleccione una opción...", disabled: true},
+				{value: "ABT", text: "Abierto"},
+				{value: "ESP", text: "En espera"},
+				{value: "FIN", text: "Finalizado"},
+			],
+			ticket: {
+				estatus: "",
+			},
+		};
+	},
+	computed: {},
+	methods: {
+		...mapActions(["obtenerTicket", "editarTicket"]),
+		guardarTicket() {
+			this.editarTicket({
+				id: this.$route.params.id,
+				params: this.ticket,
+				onComplete: (response) => {
+					this.$notify({
+						type: "success",
+						title: response.data.mensaje,
+					});
+					this.$router.push({
+						name: "TicketsPage",
+					});
+				},
+				onError: (error) => {
+					this.$notify({
+						type: "error",
+						title: error.response.data.mensaje,
+					});
+				},
+			});
+		},
+	},
+	created() {
+		this.obtenerTicket({
+			id: this.$route.params.id,
+			onComplete: (response) => {
+				Vue.set(this, "ticket", response.data);
+			},
+			onError: (err) => {
+				console.log(err);
+			},
+		});
+	},
+};
+</script>
+
+<style></style>
---
+++ client/src/views/tickets/TicketsPage.vue
@@ -0,0 +1,102 @@
+<template>
+	<div>
+		<h1>Tickets</h1>
+		<b-button variant="primary" to="/tickets/agregar"
+			><font-awesome-icon icon="plus"
+		/></b-button>
+
+		<Table emptyText="No hay tickets." :items="tickets" :fields="campos" :busy="loading">
+			<template slot="actions" slot-scope="{item}">
+				<b-button variant="success" class="me-1" @click="onCambiarEstatus(item)">
+					<font-awesome-icon icon="sync-alt" />
+				</b-button>
+				<b-button variant="primary" class="me-1" @click="onEditar(item)">
+					<font-awesome-icon icon="pen" />
+				</b-button>
+				<b-button variant="danger" @click="onEliminar(item)">
+					<font-awesome-icon icon="trash" />
+				</b-button>
+			</template>
+		</Table>
+	</div>
+</template>
+
+<script>
+import Table from "../../components/Table";
+import {mapState, mapActions} from "vuex";
+
+export default {
+	name: "TicketsPage",
+	components: {
+		Table,
+	},
+	data() {
+		return {
+			campos: [
+				{key: "id", label: "Clave", sortable: true},
+				{key: "nombre", sortable: true},
+				{key: "descripcion", sortable: true},
+				{key: "prioridad", sortable: true},
+				{key: "persona_id", sortable: true},
+				{key: "categoria_id", sortable: true},
+				{key: "actions", label: "Acciones"},
+			],
+		};
+	},
+	computed: {
+		...mapState(["tickets", "loading"]),
+	},
+	methods: {
+		...mapActions(["listarTickets", "eliminarTicket", "editarTicket"]),
+		onCambiarEstatus(item) {
+			this.$router.push({
+				name: "CambiarEstatus",
+				params: {
+					id: item.item.id,
+				},
+			});
+		},
+		onEditar(item) {
+			this.$router.push({
+				name: "EditarTicket",
+				params: {
+					id: item.item.id,
+				},
+			});
+		},
+		onEliminar(item) {
+			this.$bvModal
+				.msgBoxConfirm("Esta opción no se puede deshacer.", {
+					title: "Eliminar Ticket",
+					size: "sm",
+					buttonSize: "sm",
+					okVariant: "danger",
+					okTitle: "Aceptar",
+					cancelTitle: "Cancelar",
+					footerClass: "p-2",
+					centered: true,
+				})
+				.then((value) => {
+					if (value) {
+						this.eliminarTicket({
+							id: item.item.id,
+							onComplete: (response) => {
+								this.$notify({
+									type: "success",
+									title: response.data.mensaje,
+								});
+								this.listarTickets();
+							},
+						});
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+	},
+	mounted() {
+		this.listarTickets();
+	},
+};
+</script>
